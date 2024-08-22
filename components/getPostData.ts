d2d92cc13a7ae14b60f6939f6af898a460b754ba
tuti <tsuchi995@gmail.com>
2024-08-23 03:01:28 +0900

    fix: file path change_3

--- components/getPostData.ts
+++ components/getPostData.ts
@@ -5,7 +5,12 @@ import { notFound } from "next/navigation";
 import { revalidatePath } from "next/cache";
 
 const getPostData = (slug: string): any => {
-  const content = fs.readFileSync(`content/${slug}.md`, "utf8");
+  const folder = path.join(process.cwd(), "content");
+  const file = path.join(folder, `${slug}.md`);
+  if (!fs.existsSync(file)) {
+    throw new Error(`File not found: ${file}`);
+  }
+  const content = fs.readFileSync(file, "utf8");
   console.log(slug, "slug");
   console.log(content, "content");
   const matterResult = matter(content);
